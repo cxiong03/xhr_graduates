177747406092dea0ddf431b62e984cd8bb107737
Chang Xiong <cnxiong03@gmail.com>
2020-02-20 21:03:29 -0500

    create handlers, models, and html

--- server.js
+++ server.js
@@ -7,9 +7,17 @@ const PORT = 3000;
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.use('/', express.static('public'));
+
 const db = mongoose.connection;
 
 db.on('error', err => console.error(error));
 db.once('open', () => console.log( 'You are connected to your Database...' ));
+
+app.use(express.json());
+
+const graduatesRouter = require('./routes/profile');
+
+app.use('/api/graduates', graduatesRouter);
 
 app.listen(PORT, () => console.log(`Server listening on port ${ PORT }`));
